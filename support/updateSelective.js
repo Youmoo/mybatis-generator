30a8caa8a9a704314fb2dea18cbf2b2d4f0c6775
youmoo <youmoolee@gmail.com>
2015-05-07 23:03:20 +0800

    fix updateSelective

--- support/updateSelective.js
+++ support/updateSelective.js
@@ -8,18 +8,20 @@ module.exports = function updateSelective(tableDesc) {
     var results = tableDesc.rows.map(function (row) {
         return [
             '\n\t\t<if test="', row.javaField, ' != null">',
-            row.field, '= #{', row.javaField, '}',
+            row.field, '= #{', row.javaField, '},',
             '</if>'
         ].join('')
     });
 
+    //Here, the set element will dynamically prepend the SET keyword, and also eliminate any extraneous commas
+    // that might trail the value assignments after the conditions are applied.
     var content = [
         '<update id="updateSelective" parameterType="your.entity.Type">',
         '\n\tupdate ',
         tableDesc.table,
-        ' set ',
-        results.join(','),
-        '\n</update>'].join('');
+        '\n\t<set> ',
+        results.join(''),
+        '\n\t</set>\n</update>'].join('');
 
     return new Promise(function (res) {
         res(content);
